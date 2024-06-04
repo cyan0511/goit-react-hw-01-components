c209861ca9e678e4d4fc2b0e1fdc92ec870d031d
Cyrus Ducut <cyrus.ducut@fieldforce4.com>
2024-06-04 15:50:09 +0800

    react hw 01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import { Profile } from './profile/Profile';
+import { Statistics } from './statistics/Statistics';
+import user from '../assets/json/user.json';
+import stats from '../assets/json/data.json';
+import friends from '../assets/json/friends.json';
+import transactions from '../assets/json/transactions.json';
+import { FriendList } from './friend-list/FriendList';
+import { TransactionHistory } from './transaction-history/TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="container">
+      <div className="info-container">
+        <div className="friends-container">
+          <Profile user={user} />
+          <FriendList friends={friends} />
+        </div>
+        <Statistics title="User Stats"  stats={stats} />
+      </div>
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
